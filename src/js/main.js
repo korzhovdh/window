9a82450e302ef1899c839954ac7d964b7c632698
gunther <korzhovdh@gmail.com>
2023-08-01 17:47:52 +0200

    timer script added

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,13 @@ import modals from "./modules/modals";
 import tabs from "./modules/tabs";
 import forms from "./modules/forms";
 import changeModalState from "./modules/changeModalState";
+import timer from "./modules/timer";
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
 
     let modalState = {};
+    let deadline = '2023-12-08';
 
 
     changeModalState(modalState);
@@ -15,5 +17,6 @@ window.addEventListener('DOMContentLoaded', () => {
     tabs('.glazing_slider' ,'.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider' ,'.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block' );
+    timer('.container1', deadline);
     forms(modalState);
 });
